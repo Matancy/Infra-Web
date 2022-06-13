910793f18b46bfdb0ebafc7429a5d1e832759d8d
Matancy <24440469+Matancy@users.noreply.github.com>
2022-06-13 11:52:27 +0200

    Update sql and add table counters and newsletter

--- create.sql
+++ create.sql
@@ -1,10 +1,27 @@
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `verified` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS nasa.users;
+CREATE TABLE IF NOT EXISTS nasa.users (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  created_at datetime NOT NULL,
+  token varchar(255) NOT NULL,
+  verified tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS nasa.counters;
+CREATE TABLE IF NOT EXISTS nasa.counters (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  value int(11) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS nasa.newsletter;
+CREATE TABLE IF NOT EXISTS nasa.newsletter (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  email varchar(255) NOT NULL,
+  inscription int (11) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
