e165c9e16009a73d1b60be806dc495219f149691
cleomc <95679288+cleomc@users.noreply.github.com>
2022-06-13 12:06:51 +0200

    wip

---
+++ area51.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confidential - Area 51</title>
+    <link rel="shortcut icon" href="assets/ico/nasa-logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="style/login.css">
+</head>
+<body>
+    <?php include('php/navBar.php') ?>
+    <h1>Do not leak this informations</h1>
+    <?php 
+        echo '<h4 style="color : white">User Adress: ' . $_SERVER['REMOTE_ADDR'].'</h1>';
+        //get client gps location
+
+        $new_arr[]= unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip=45.147.98.172'));
+        echo "<h4 style=\"color : white\">Latitude:".$new_arr[0]['geoplugin_latitude']." and Longitude:".$new_arr[0]['geoplugin_longitude'].'</h4>';        
+        
+    ?>
+</body>
+</html>
