9a4bb6371f5a27d730c607530f0fc421215e9106
Matancy <24440469+Matancy@users.noreply.github.com>
2022-06-14 06:38:18 +0200

    Update database login

--- php/database.php
+++ php/database.php
@@ -1,13 +1,13 @@
 <?php 
     $servername = "localhost"; // Adress Bdd
     $user = "root"; // userName Bdd
-    $pass = ""; // Password Bdd
+    $pass = "ZM6&vk53WG&Zft"; // Password Bdd
     $dbname = "nasa"; // Nom de la Bdd
     try {
         $bdd = new PDO("mysql:host=$servername;dbname=$dbname; charset=utf8", $user, $pass);
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
+        die("Database connection failed: " . $e->getMessage());
     }
 ?>
