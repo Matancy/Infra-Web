7c304acba5db5c736dc7e355ce5fe13eed1ff982
cleomc <95679288+cleomc@users.noreply.github.com>
2022-06-14 18:32:11 +0200

    import the good css

--- area51.php
+++ area51.php
@@ -6,19 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Confidential - Area 51</title>
     <link rel="shortcut icon" href="assets/ico/nasa-logo.png" type="image/x-icon">
-    <link rel="stylesheet" href="style/login.css">
+    <link rel="stylesheet" href="style/charte-graphique.css">
+    <link rel="stylesheet" href="style/area51.css">
+
 </head>
 <body>
     <?php include('php/navBar.php') ?>
     <h1>Do not leak this informations</h1>
     <?php 
         include('php/distance.php');
-        echo '<h4 style="color : white">User Adress: ' . $_SERVER['REMOTE_ADDR'].'</h1>';
         //get client gps location
 
         $new_arr[]= unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip=45.147.98.172'));
         //echo "<h4 style=\"color : white\">Latitude:".$new_arr[0]['geoplugin_latitude']." and Longitude:".$new_arr[0]['geoplugin_longitude'].'</h4>';        
-        echo '<h4 style="color : white">Distance: ' . distance($new_arr[0]['geoplugin_latitude'], $new_arr[0]['geoplugin_longitude'], $_SERVER['REMOTE_ADDR'], $_SERVER['REMOTE_ADDR']) . ' km</h4>';
+        echo '<h4 style="color : red">Distance: ' . distance($new_arr[0]['geoplugin_latitude'], $new_arr[0]['geoplugin_longitude'], $_SERVER['REMOTE_ADDR'], $_SERVER['REMOTE_ADDR']) . ' km</h4>';
     ?>
 </body>
 </html>
