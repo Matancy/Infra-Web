58f7234bdcaa5d59d2b79dd2ea28893ccb325778
cleomc <95679288+cleomc@users.noreply.github.com>
2022-06-15 19:01:42 +0200

    Update database.php

--- php/database.php
+++ php/database.php
@@ -2,8 +2,8 @@
     session_start(); // Start session
     $servername = "localhost"; // Adress Bdd
     $user = "root"; // userName Bdd
-    $pass = ""; // password Bdd
-    //$pass = "ZM6&vk53WG&Zft"; // Password Bdd 
+    //$pass = ""; // password Bdd
+    $pass = "ZM6&vk53WG&Zft"; // Password Bdd 
     $dbname = "nasa"; // Nom de la Bdd
     try {
         $bdd = new PDO("mysql:host=$servername;dbname=$dbname; charset=utf8", $user, $pass);
