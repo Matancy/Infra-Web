cdc88986690077c6002d2184d64733f6d4a71cd9
Matancy <24440469+Matancy@users.noreply.github.com>
2022-06-15 21:39:51 +0200

    Update remove_users.php

--- admin/remove_users.php
+++ admin/remove_users.php
@@ -2,7 +2,7 @@
 
 require '/var/www/html/php/database.php';
 
-$req = $bdd->prepare('DELETE * FROM users WHERE created_at < NOW() - INTERVAL 1 DAY AND verified = 0');
+$req = $bdd->prepare('DELETE FROM users WHERE created_at < NOW() - INTERVAL 1 DAY AND verified = 0');
 $req->execute();
 
 exit();
