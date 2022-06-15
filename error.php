0875c9e7aa8db6a4766e3f345e826e39b6fe7cda
maguezennec <mateo.guezennec@etudiant.univ-rennes1.fr>
2022-06-15 15:20:48 +0200

    Change Error's page

--- error.php
+++ error.php
@@ -1,5 +1,69 @@
 <?php 
     include 'php/database.php'; // Include database connection
+
+    $error = ""; // Initialize error variable
+    $message = "No error"; // Initialize message variable
+
+    if(isset($_GET['err'])) {
+        $error = $_GET['err'];
+    }
+
+    switch ($error) {
+        case '400':
+            $message = "Bad request";
+            break;
+        case '401':
+            $message = "Unauthorized";
+            break;
+        case '403':
+            $message = "Forbidden";
+            break;
+        case '404':
+            $message = "page not found";
+            break;
+        case '405':
+            $message = "Method not allowed";
+            break;
+        case '406':
+            $message = "Not acceptable";
+            break;
+        case '407':
+            $message = "Proxy authentication required";
+            break;
+        case '408':
+            $message = "Request timeout";
+            break;
+        case '409':
+            $message = "Conflict";
+            break;
+        case '410':
+            $message = "Gone";
+            break;
+        case '411':
+            $message = "Length required";
+            break;
+        case '412':
+            $message = "Precondition failed";
+            break;
+        case '500':
+            $message = "Internal server error";
+            break;
+        case '501':
+            $message = "Not implemented";
+            break;
+        case '502':
+            $message = "Bad gateway";
+            break;
+        case '503':
+            $message = "Service unavailable";
+            break;
+        case '504':
+            $message = "Gateway timeout";
+            break;
+        default:
+            $message = "No error";
+            break;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -52,8 +116,8 @@
 </head>
 <body>
     <div class="container">
-        <h1>Error 404</h1>
-        <p>page not found</p>
+        <h1>Error <?php echo $error; ?></h1>
+        <p><?php echo $message; ?></p>
         <i class="fa-solid fa-bug"></i><br>
         <a href="index.php"><button>Back To Earth 🌎</button></a>
     </div>
