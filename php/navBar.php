b0ac5e96a4f7c9c5caf7989d344da3b1df0303d0
Matancy <24440469+Matancy@users.noreply.github.com>
2022-06-15 16:27:34 +0200

    Update php session

--- php/navBar.php
+++ php/navBar.php
@@ -1,9 +1,9 @@
 <nav id="navbar">
     <ul>
-        <li class="left"><img src="./assets/ico/nasa-logo.png" alt="" /><a href="index.php">Home</a></li>
+        <li class="left"><img src="./assets/ico/nasa-logo.png" alt=""/><a href="index">Home</a></li>
         <?php
-            if(isset($_SESSION['id'])) {
-                echo '<li class="left">Hi, ' . $_SESSION['name'] . '</li>';
+            if(isset($_SESSION['user']['id'])) {
+                echo '<li class="left">Hi, ' . $_SESSION['user']['name'] . '</li>';
                 echo '<li class="right"><a href="/logout">Logout</a></li>';
                 echo '<li class="right"><a href="/area51">Area 51</a></li>';
             } else {
